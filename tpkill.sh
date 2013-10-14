1d2dc084a8fbdb9d895e877cdf0d5e48502a8fe9
Blayne Campbell <blayne@blaynecampbell.com>
2013-10-13 21:53:54 -0600

    Update tpkill.sh

--- tpkill.sh
+++ tpkill.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# This script will find all the PID's of the PROCESS_NAME processes
+# that have been running for 10 minutes or more and send each PID a
+# SIGKILL.
  
 PIDS="`ps ax | egrep "PROCESS_NAME" | grep ":10" | awk '{print $1}'`"
 for i in ${PIDS}; do { echo "Killing $i"; kill -9 $i; }; done;
